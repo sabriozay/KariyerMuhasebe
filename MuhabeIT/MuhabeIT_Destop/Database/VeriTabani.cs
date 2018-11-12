c891c1553736c8cf017562d5f1471ed9c7ab4198
sabriozay <36267741+sabriozay@users.noreply.github.com>
2018-11-12 21:52:54 +0300

    12112018
    Giris ekranı yapıldı (githup tekrar)

--- MuhabeIT/MuhabeIT_Destop/Database/VeriTabani.cs
+++ MuhabeIT/MuhabeIT_Destop/Database/VeriTabani.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,10 @@ using MuhabeIT_Destop.Models;
 
 namespace MuhabeIT_Destop.Database
 {
-  
+    // ekleme silme güncelleme yapılacagı zaman cmd methodu kullanılacak
+    // bir tablodaki bütün bilgiler icin  VeriTablosuCek methodu kullanılacak
+    //bir tablodaki bir satır icin VeriSatiriCek methodu kullanılacak
+    // bir tablodki bir satırın bir veriri icin vericek methodu kullanilacak
 
 
     class VeriTabani
@@ -22,15 +26,16 @@ namespace MuhabeIT_Destop.Database
         }
         public SqlConnection baglan()
         {
+            VeriDb = "MuhasebeDb";
             String BaglantiYol = "Data Source=www.portsispos.com;Initial Catalog = '" + VeriDb + "'; User ID = sa; Password=@p0rTs|s897*@;";
             SqlConnection baglanti = new SqlConnection(BaglantiYol);
             baglanti.Open();
             return baglanti;
         }
-        public int cmd(string sqlcumle)
+        public int cmd(string Sorgu)
         {
             SqlConnection baglan = this.baglan();
-            SqlCommand sorgu = new SqlCommand(sqlcumle, baglan);
+            SqlCommand sorgu = new SqlCommand(Sorgu, baglan);
             int sonuc = 0;
             try
             {
@@ -38,7 +43,7 @@ namespace MuhabeIT_Destop.Database
             }
             catch (SqlException ex)
             {
-                throw new Exception(ex.Message + "(" + sqlcumle + ")");
+                throw new Exception(ex.Message + "(" + Sorgu + ")");
             }
             sorgu.Dispose();
             baglan.Close();
@@ -46,13 +51,55 @@ namespace MuhabeIT_Destop.Database
             return sonuc;
         }
 
-        public Login LoginQuary(string kullaniciAdi,String sifre)
+        public DataTable VeriTablosuCek(string sorgu)
         {
-            string sql = "select * from Login where KullaniciAdi='"+kullaniciAdi+"' And Pw='"+sifre+"'";
-            SqlConnection baglanti =this.baglan();
-            SqlDataAdapter adapter = new SqlDataAdapter(sql, baglanti);
-            Login lg = new Login();            
-            return lg;
+            SqlConnection baglanti = this.baglan();
+            SqlDataAdapter adapter = new SqlDataAdapter(sorgu, baglanti);
+            DataTable dt = new DataTable();
+            try
+            {
+                adapter.Fill(dt);
+            }
+            catch (SqlException hata)
+            {
+
+                throw new Exception(hata.Message +"("+sorgu+")");
+            }
+            adapter.Dispose();
+            baglanti.Close();
+            baglanti.Dispose();
+            return dt;
+
+        }
+
+        public DataRow VeriSatiriCek(string Sorgu)
+        {
+            DataTable table = this.VeriTablosuCek(Sorgu);
+            DataRow satir;
+            if (table.Rows.Count==0)
+            {
+                satir = null;
+            }
+            else
+            {
+                satir = table.Rows[0];
+            }
+            return satir;
+        }
+
+        public string Vericek(string Sorgu)
+        {
+            DataTable table = this.VeriTablosuCek(Sorgu);
+            string veri;
+            if (table.Rows.Count==0)
+            {
+                veri = null;
+            }
+            else
+            {
+                veri = table.Rows[0][0].ToString();
+            }
+            return veri;
         }
 
 
--- MuhabeIT/MuhabeIT_Destop/Form1.Designer.cs
+++ MuhabeIT/MuhabeIT_Destop/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -47,16 +48,17 @@
             this.textBox2.Location = new System.Drawing.Point(83, 49);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 0;
+            this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(83, 84);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 39);
-            this.button1.TabIndex = 1;
+            this.button1.TabIndex = 2;
             this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -76,18 +78,29 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Şifre";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Giris";
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 161);
+            this.ClientSize = new System.Drawing.Size(259, 157);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Giriş";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +113,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
