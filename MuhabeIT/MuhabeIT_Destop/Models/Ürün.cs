e6b6e4fe3139e279fc4b5398b5f28173b628c646
Muhammed Karcı <44744148+muhammed1834@users.noreply.github.com>
2018-11-23 16:43:05 +0300

    ürün ekle
    Ürün ekle models klasörüne eklendi kodlar yazıldı lakin ürün ekle butonunda metod tanımında bir sıkıntı var bakabilirmisin sabri whatsaptan atıyorum resimleri

---
+++ MuhabeIT/MuhabeIT_Destop/Models/Ürün.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuhabeIT_Destop.Models
+{
+    class Ürün
+    {
+        public int id { get; set; }
+        public String Ürün_Adi{ get; set; }
+        public String Ürün_Adeti { get; set; }
+        public String Ürün_Birimi { get; set; }
+        public String Ürün_Birim_Fiyat { get; set; }
+        public String Ürün_SeriNo { get; set; }
+        public String Aciklama { get; set; }
+    }
+}
--- MuhabeIT/MuhabeIT_Destop/UC/ÜrünEkle.cs
+++ MuhabeIT/MuhabeIT_Destop/UC/ÜrünEkle.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using MuhabeIT_Destop.UC;
+using MuhabeIT_Destop.Models;
+using MuhabeIT_Destop.Database;
 
 namespace MuhabeIT_Destop.UC
 {
     public partial class ÜrünEkle : UserControl
     {
+        VeriTabani db = new VeriTabani();
+        Ürün ürn = new Ürün();
+        
         public ÜrünEkle()
         {
             InitializeComponent();
@@ -19,6 +26,29 @@ namespace MuhabeIT_Destop.UC
 
         private void Ürnekle_Click(object sender, EventArgs e)
         {
+            ürn.Ürün_Adi = textBox1.Text;
+            ürn.Ürün_Adeti = textBox2.Text;
+            ürn.Ürün_Birimi = textBox3.Text;
+            ürn.Ürün_Birim_Fiyat = textBox4.Text;
+            ürn.Ürün_SeriNo = textBox5.Text;
+            ürn.Aciklama = textBox6.Text;
+            if (ürn.Ürün_Adi != "" && ürn.Ürün_Adeti != "" && ürn.Ürün_Birimi != "" && ürn.Ürün_Birim_Fiyat != "" && ürn.Ürün_SeriNo != "" && ürn.Aciklama != "")
+            {
+                int gelen = db.cmd(Sorgu:)
+                if (gelen == 1)
+                {
+                    MessageBox.Show(ürn.Ürün_Adi + "Ürünü Kayıt Edildi");
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Başarısız Lütfen Bilgilerinizi Kontrol Edin");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Alanları Boş Bırakmayınız..!");
+
+            }
 
         }
 
