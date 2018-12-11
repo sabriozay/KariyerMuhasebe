df5aefd41d82c0367b597f2113931cd2393755a3
Muhammed Karcı <44744148+muhammed1834@users.noreply.github.com>
2018-12-11 17:23:12 +0300

    d
    deneme

--- MuhabeIT/MuhabeIT_Destop/UCbaslik/MüsteriSecenek.cs
+++ MuhabeIT/MuhabeIT_Destop/UCbaslik/MüsteriSecenek.cs
@@ -23,7 +23,7 @@ namespace MuhabeIT_Destop.UCbaslik
         MüsteriEkle ekle = new MüsteriEkle();
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
             home.panel3.Controls.Clear();
             home.panel3.Controls.Add(ekle);
             ekle.Dock = DockStyle.Fill;
